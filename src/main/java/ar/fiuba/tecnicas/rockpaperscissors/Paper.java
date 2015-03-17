6c3a0f9192e1bd3c669050a6e1cf3db5751fb79a
Mariano Rodriguez <marianknot92@gmail.com>
2015-03-17 12:39:35 -0300

    agrego los metodos vs de Paper

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Paper.java
@@ -2,4 +2,15 @@ package ar.fiuba.tecnicas.rockpaperscissors;
 
 public class Paper {
 
+	Paper vs(Paper otherPaper) {
+		return this;
+	}
+
+	Paper vs(Rock rock) {
+		return this;
+	}
+
+	Scissors vs(Scissors scissors) {
+		return scissors;
+	}
 }
